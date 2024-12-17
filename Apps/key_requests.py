09fe36259c225c671515455f6efcb16f33278ea3
anwinn03 <atn24@georgetown.edu>
2024-12-17 01:04:58 -0500

    update #2

--- Apps/key_requests.py
+++ Apps/key_requests.py
@@ -4,4 +4,4 @@ load_dotenv() # look in the .env file for env vars
 
 API_KEY = os.getenv("ALPHAVANTAGE_API_KEY", default="demo")
 SHEETS_API_KEY = os.getenv("SHEETS_API_KEY", default="demo")
-SHEETS_KEY = os.getenv("SHEET_KEY", default="demo")
+SHEETS_KEY = os.getenv("SHEETS_KEY", default="demo")
--- test.py
+++ test.py
@@ -1,5 +1,14 @@
+import pandas as pd
+import requests
+import gspread
+import plotly.express as px
 from Apps.key_requests import API_KEY
 from Apps.key_requests import SHEETS_API_KEY
 from Apps.key_requests import SHEETS_KEY
 
-print(API_key, '/n', SHEETS_API_KEY, '/n', SHEETS_KEY, '/n')
+def fetch_sheets_to_pandas(api_key, sheets_key):
+    gc = gspread.api_key(api_key)
+    sh = gc.open_by_key(sheets_key)
+    spreadsheet = sh.sheet1.get_all_records(head=1)
+    return pd.DataFrame(spreadsheet)
+
