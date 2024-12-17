942002bef32a4ad0ec53f73bcdf47ea31d607658
cl1354 <cl1354@georgetown.edu>
2024-12-16 20:10:33 -0500

    License and misc. formatting functions

--- app/portfolio.py
+++ app/portfolio.py
@@ -10,6 +10,14 @@ from app.alpha_service import API_KEY
 def format_usd(my_price):
     return f"${float(my_price):,.2f}"
 
+def format_value(number):
+    number = float(number)
+    formatted = format(number, '.3f')
+    return float(formatted)
+
+def year(date):
+    year = date[:4]
+    return year
 
 def fetch_stocks_csv(symbol):
     request_url = f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol={symbol}&apikey={API_KEY}&outputsize=full&datatype=csv"
